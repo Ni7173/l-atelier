eb90d306d377580abe64ba305599b6649d4c2af7
Ni7173 <nino.bravais20@gmail.com>
2024-05-22 15:47:51 +0200

    bug du logo qui bouge

--- js/global.js
+++ js/global.js
@@ -2,9 +2,20 @@ function menuMobile() {
     const btn = document.querySelector('.burger');
     const header = document.querySelector('header');
     const links = document.querySelectorAll('.navbar a');
+    let logo = document.querySelector('.logo a');
+    let logoState;
+
 
     btn.addEventListener('click', () => {
         header.classList.toggle('show-nav');
+        if (logo.classList == 'logo--is-translated') {
+            logo.classList.remove('logo--is-translated');
+            logoState = "changed";
+            return
+        }
+        if (logoState === "changed") {
+            logo.classList.add('logo--is-translated');
+        }
     })
     links.forEach(link =>
         link.addEventListener('click', () => {
@@ -14,15 +25,6 @@ function menuMobile() {
 }
 menuMobile();
 
-// const loading = () => {
-
-//     const loader = document.getElementById('loader')
-
-//     window.addEventListener('load', () => {
-//         loader.classList.remove('is-loading')
-//     })
-// }
-// loading();
 
 // if (window.history.replaceState) {
 //     window.history.replaceState(null, null, window.location.pathname.slice(0, -5));
