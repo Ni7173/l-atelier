876a54234cc156d592f5cd1ca63318515ec4cb73
Ni7173 <nino.bravais20@gmail.com>
2024-05-22 16:09:12 +0200

    logo qui bouge fixed + disable scrolling + position logo petit ecran

--- js/global.js
+++ js/global.js
@@ -5,9 +5,30 @@ function menuMobile() {
     let logo = document.querySelector('.logo a');
     let logoState;
 
+    const disableScrolling = () => {
+        var x = window.scrollX;
+        var y = window.scrollY;
+        window.onscroll = function () { window.scrollTo(x, y); };
+    }
+
+    const enableScrolling = () => {
+        window.onscroll = function () { };
+    }
+
+    const scrollManager = () => {
+        if (header.classList == "show-nav") {
+            disableScrolling();
+        } else {
+            enableScrolling();
+        }
+    }
+
 
     btn.addEventListener('click', () => {
         header.classList.toggle('show-nav');
+
+        scrollManager();
+
         if (logo.classList == 'logo--is-translated') {
             logo.classList.remove('logo--is-translated');
             logoState = "changed";
@@ -15,6 +36,7 @@ function menuMobile() {
         }
         if (logoState === "changed") {
             logo.classList.add('logo--is-translated');
+            logoState = 0;
         }
     })
     links.forEach(link =>
