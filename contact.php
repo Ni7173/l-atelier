f3176818c17ae18b587866adacad8c25448d3539
Ni7173 <nino.bravais20@gmail.com>
2024-06-14 10:32:31 +0200

    php 3

--- contact.php
+++ contact.php
@@ -1,22 +1,23 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $message = $_POST['message'];
-    $tel = $_POST['tel'];
-    $email = $_POST['email'];
+    $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+    $message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
+    $tel = htmlspecialchars($_POST['tel'], ENT_QUOTES, 'UTF-8');
+    $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
 
     $to_email = "contact@latelier-8.fr";
     
     $subject = "Nouveau message de $name";
 
     $email_message = "Name: $name\n";
-    $email_message .= "Message: $message\n";
     $email_message .= "Telephone: $tel\n";
-    $email_message .= "Email: $email\n";
+    $email_message .= "Email: $email\n\n";
+    $email_message .= "Message: $message\n";
 
-    $headers = "From: ".$email;
-    // $headers .= "Reply-To: $email\r\n";
-    // $headers .= "X-Mailer: PHP/" . phpversion();
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
 
     if (mail($to_email, $subject, $email_message, $headers)) {
         header("Location: /pages/thank-you.html");
