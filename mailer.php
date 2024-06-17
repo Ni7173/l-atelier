1c83fccdc55878a9cd245a0fe3c87f96cd7a988f
Ni7173 <nino.bravais20@gmail.com>
2024-06-17 18:07:57 +0200

    essai mailer 3

--- mailer.php
+++ mailer.php
@@ -1,5 +1,5 @@
 <?php
-require '/home/annebonnet/secure/config.php';
+require '../secure/config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
