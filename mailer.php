c56925669b9e80d7471c0ce3dc42e32cbedc22ce
Ni7173 <nino.bravais20@gmail.com>
2024-06-20 17:59:37 +0200

    php ta mere

--- mailer.php
+++ mailer.php
@@ -4,9 +4,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// require 'vendor/phpmailer/src/Exception.php';
-// require 'vendor/phpmailer/src/PHPMailer.php';
-// require 'vendor/phpmailer/src/SMTP.php';
 require '../secure/config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
