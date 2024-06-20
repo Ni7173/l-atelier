70e369011124cb929de06a478ab99897aa6e1970
Ni7173 <nino.bravais20@gmail.com>
2024-06-20 17:08:57 +0200

    update mailer 3

--- mailer.php
+++ mailer.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/../secure/config.php';
+require __DIR__ . 'vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
