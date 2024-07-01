193737fd719d0c645090160fd08af44b63e7d15f
Ni7173 <nino.bravais20@gmail.com>
2024-07-01 11:33:37 +0200

    insertion html et css js

--- js/feed-insta.js
+++ js/feed-insta.js
@@ -1,6 +1,7 @@
-const accessToken = 'YOUR_ACCESS_TOKEN';
-const userId = 'YOUR_USER_ID';  // Remplacez par votre ID utilisateur Instagram
+const accessToken = 'IGQWRQUy1YREgtWjBkalhKY0FaQmplWFdMLXdrNElUMnhNZAW43R2Q5NnNZAaV8tX1A4ODQwUjlsMDhFd1lFQnZA5NnBXNzVKbThSc0hCQ3h0eE9WM0hEeWJYLXFWOUIxRmZANUmM2cDhGYzJwUEt4ZAnh4ME9tczRDVFkZD';
+const userId = '7675127815910055';
 const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
+
 
 fetch(url)
     .then(response => response.json())
@@ -10,9 +11,10 @@ fetch(url)
             const postElement = document.createElement('div');
             postElement.className = 'instagram-post';
             postElement.innerHTML = `
-                        <a href="${post.permalink}" target="_blank">
-                            <img src="${post.media_url}" alt="${post.caption}" />
+                        <a class="absoluted" href="${post.permalink}" target="_blank">
+                        <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption}" />
                         </a>
+                        <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
                     `;
             feedContainer.appendChild(postElement);
         });
