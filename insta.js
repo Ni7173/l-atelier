8934cf802ae697800560fbb959ff08a2d9e42f07
Ni7173 <nino.bravais20@gmail.com>
2024-07-06 09:48:50 +0200

    insta feed

---
+++ insta.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
+const userId = process.env.INSTAGRAM_USER_ID;
+const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const feedContainer = document.getElementById('ig-feed');
+        data.data.forEach(post => {
+            const postElement = document.createElement('div');
+            postElement.className = 'instagram-post';
+            postElement.innerHTML = `
+                        <a class="absoluted" href="${post.permalink}" target="_blank">
+                        <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption}" />
+                        </a>
+                        <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
+                    `;
+            feedContainer.appendChild(postElement);
+        });
+    })
+    .catch(error => console.error('Error fetching Instagram posts:', error));
