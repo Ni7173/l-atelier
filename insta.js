7d79e6b4ff23c1d229165504bd3c8e74d2364d8e
Ni7173 <nino.bravais20@gmail.com>
2024-07-06 10:02:24 +0200

    insta feed 2

--- insta.js
+++ insta.js
@@ -1,26 +1,32 @@
-const dotenv = ('dotenv');
-
-dotenv.config();
-
-
-const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
-const userId = process.env.INSTAGRAM_USER_ID;
-const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
-
-fetch(url)
+fetch('/config')
     .then(response => response.json())
-    .then(data => {
-        const feedContainer = document.getElementById('ig-feed');
-        data.data.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.className = 'instagram-post';
-            postElement.innerHTML = `
+    .then(config => {
+        const accessToken = config.accessToken;
+        const userId = config.userId;
+        const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
+
+
+
+        // const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
+        // const userId = process.env.INSTAGRAM_USER_ID;
+        // const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const feedContainer = document.getElementById('ig-feed');
+                data.data.forEach(post => {
+                    const postElement = document.createElement('div');
+                    postElement.className = 'instagram-post';
+                    postElement.innerHTML = `
                         <a class="absoluted" href="${post.permalink}" target="_blank">
                         <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption}" />
                         </a>
                         <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
                     `;
-            feedContainer.appendChild(postElement);
-        });
+                    feedContainer.appendChild(postElement);
+                });
+            })
+            .catch(error => console.error('Error fetching Instagram posts:', error));
     })
-    .catch(error => console.error('Error fetching Instagram posts:', error));
+    .catch(error => console.error('Error fetching configuration:', error));
--- server.js
+++ server.js
@@ -1,50 +1,68 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
+const express = require('express');
+const dotenv = require('dotenv');
 dotenv.config();
 
-
 const app = express();
-app.use(cors());
 const PORT = process.env.PORT || 3000;
 
-// Middleware pour parser les JSON
-app.use(bodyParser.json());
-
-// Route pour échanger le code d'autorisation contre un jeton d'accès
-app.post('/exchange-code', async (req, res) => {
-    const authorizationCode = req.body.code;
-
-    if (authorizationCode) {
-        const params = new URLSearchParams();
-        params.append('client_id', process.env.INSTAGRAM_CLIENT_ID);
-        params.append('client_secret', process.env.INSTAGRAM_CLIENT_SECRET);
-        params.append('grant_type', 'authorization_code');
-        params.append('redirect_uri', process.env.INSTAGRAM_REDIRECT_URI);
-        params.append('code', authorizationCode);
-
-        try {
-            const response = await fetch('https://api.instagram.com/oauth/access_token', {
-                method: 'POST',
-                body: params
-            });
-            const data = await response.json();
-            if (data.access_token) {
-                res.json({ success: true, data });
-            } else {
-                res.json({ success: false, error: data.error_message });
-            }
-        } catch (error) {
-            res.status(500).json({ success: false, error: 'Server error' });
-        }
-    } else {
-        res.status(400).json({ success: false, error: 'Authorization code not provided' });
-    }
+// Endpoint pour envoyer des informations sensibles au client
+app.get('/config', (req, res) => {
+    res.json({ accessToken: process.env.INSTAGRAM_ACCESS_TOKEN });
+    res.json({ userId: process.env.INSTAGRAM_USER_ID });
+    res.json({ redirectUri: process.env.INSTAGRAM_REDIRECT_URI });
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// import express from 'express';
+// import fetch from 'node-fetch';
+// import bodyParser from 'body-parser';
+// import dotenv from 'dotenv';
+// import cors from 'cors';
+
+// dotenv.config();
+
+
+// const app = express();
+// app.use(cors());
+// const PORT = process.env.PORT || 3000;
+
+// // Middleware pour parser les JSON
+// app.use(bodyParser.json());
+
+// // Route pour échanger le code d'autorisation contre un jeton d'accès
+// app.post('/exchange-code', async (req, res) => {
+//     const authorizationCode = req.body.code;
+
+//     if (authorizationCode) {
+//         const params = new URLSearchParams();
+//         params.append('client_id', process.env.INSTAGRAM_CLIENT_ID);
+//         params.append('client_secret', process.env.INSTAGRAM_CLIENT_SECRET);
+//         params.append('grant_type', 'authorization_code');
+//         params.append('redirect_uri', process.env.INSTAGRAM_REDIRECT_URI);
+//         params.append('code', authorizationCode);
+
+//         try {
+//             const response = await fetch('https://api.instagram.com/oauth/access_token', {
+//                 method: 'POST',
+//                 body: params
+//             });
+//             const data = await response.json();
+//             if (data.access_token) {
+//                 res.json({ success: true, data });
+//             } else {
+//                 res.json({ success: false, error: data.error_message });
+//             }
+//         } catch (error) {
+//             res.status(500).json({ success: false, error: 'Server error' });
+//         }
+//     } else {
+//         res.status(400).json({ success: false, error: 'Authorization code not provided' });
+//     }
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
