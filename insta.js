afe446abbaaf8555d5a49e5050982b7e0d197d20
Ni7173 <nino.bravais20@gmail.com>
2024-07-14 17:07:10 +0200

    essai insta

--- insta.js
+++ insta.js
@@ -1,7 +1,27 @@
-const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
+// const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
 
 
-fetch(url)
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         const feedContainer = document.getElementById('ig-feed');
+//         data.data.forEach(post => {
+//             const postElement = document.createElement('div');
+//             postElement.className = 'instagram-post';
+//             postElement.innerHTML = `
+//                     <a class="absoluted" href="${post.permalink}" target="_blank">
+//                     <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption}" />
+//                     </a>
+//                     <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
+//                 `;
+//             feedContainer.appendChild(postElement);
+//         });
+//     })
+//     .catch(error => console.error('Error fetching Instagram posts:', error));
+
+const apiUrl = '/instagram/posts';
+
+fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         const feedContainer = document.getElementById('ig-feed');
@@ -9,11 +29,11 @@ fetch(url)
             const postElement = document.createElement('div');
             postElement.className = 'instagram-post';
             postElement.innerHTML = `
-                    <a class="absoluted" href="${post.permalink}" target="_blank">
-                    <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption}" />
-                    </a>
-                    <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
-                `;
+                <a class="absoluted" href="${post.permalink}" target="_blank">
+                <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption}" />
+                </a>
+                <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
+            `;
             feedContainer.appendChild(postElement);
         });
     })
