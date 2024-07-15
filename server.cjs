52f7ccc0a200e0f7cc6453048c7059007bb0c1a4
Ni7173 <nino.bravais20@gmail.com>
2024-07-15 09:27:42 +0200

    server

--- server.cjs
+++ server.cjs
@@ -1,20 +1,22 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: '/home/u672716419/domains/latelier-8.fr/secure/.env' }); const express = require('express');
-const fetch = require('node-fetch');
 const path = require('path');
+dotenv.config({ path: '/home/u672716419/domains/latelier-8.fr/secure/.env' });
+
+const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Importation dynamique de node-fetch
-import('node-fetch').then((fetch) => {
+(async () => {
+    const fetch = (await import('node-fetch')).default;
+
     const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
     const userId = process.env.INSTAGRAM_USER_ID;
     const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
 
     app.get('/instagram/posts', async (req, res) => {
         try {
-            const response = await fetch.default(url); // Utilisation de fetch.default()
+            const response = await fetch(url);
             const data = await response.json();
             res.json(data);
         } catch (error) {
@@ -22,10 +24,8 @@ import('node-fetch').then((fetch) => {
             res.status(500).json({ error: 'Échec de récupération des publications Instagram' });
         }
     });
-}).catch((err) => {
-    console.error('Erreur lors de l\'importation de node-fetch :', err);
-});
 
-app.listen(port, () => {
-    console.log(`Serveur démarré sur le port ${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Serveur démarré sur le port ${port}`);
+    });
+})();
