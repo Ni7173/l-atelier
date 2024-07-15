b96c8d584c729130da7802dcfe9ef047184505fa
Ni7173 <nino.bravais20@gmail.com>
2024-07-15 09:07:14 +0200

    server

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Importation dynamique de node-fetch
+import('node-fetch').then((fetch) => {
+    const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
+    const userId = process.env.INSTAGRAM_USER_ID;
+    const url = `https://graph.instagram.com/${userId}/media?fields=id,caption,media_url,permalink&access_token=${accessToken}`;
+
+    app.get('/instagram/posts', async (req, res) => {
+        try {
+            const response = await fetch.default(url); // Utilisation de fetch.default()
+            const data = await response.json();
+            res.json(data);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des publications Instagram :', error);
+            res.status(500).json({ error: 'Échec de récupération des publications Instagram' });
+        }
+    });
+}).catch((err) => {
+    console.error('Erreur lors de l\'importation de node-fetch :', err);
+});
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+});
