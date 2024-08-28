8871d62e0c689bef7b51877146407b828ebd11d8
Ni7173 <nino.bravais20@gmail.com>
2024-08-28 10:43:58 +0200

    update css et js

--- insta.js
+++ insta.js
@@ -1,35 +1,4 @@
-// const apiUrl = '/instagram/posts';
-
-// console.log('Fetching data from:', apiUrl);
-
-// fetch(apiUrl)
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Network response was not ok ' + response.statusText);
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         // console.log('Data received:', data);
-
-//         const feedContainer = document.getElementById('ig-feed');
-//         data.data.forEach(post => {
-//             const postElement = document.createElement('div');
-//             postElement.className = 'instagram-post';
-//             postElement.innerHTML = `
-//                 <a class="absoluted" href="${post.permalink}" target="_blank">
-//                 <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption} post instagram l'Atelier 8" />
-//                 </a>
-//                 <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
-//             `;
-//             feedContainer.appendChild(postElement);
-//         });
-//     })
-//     .catch(error => console.error('Error fetching Instagram posts:', error));
-
 const apiUrl = '/instagram/posts';
-
-console.log('Fetching data from:', apiUrl);
 
 fetch(apiUrl)
     .then(response => {
