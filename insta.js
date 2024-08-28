3db9a427ce3e7d982ea4dc2a7e9500b4b01d4caf
Ni7173 <nino.bravais20@gmail.com>
2024-08-28 10:37:25 +0200

    ajout message erreur insta

--- insta.js
+++ insta.js
@@ -1,3 +1,32 @@
+// const apiUrl = '/instagram/posts';
+
+// console.log('Fetching data from:', apiUrl);
+
+// fetch(apiUrl)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok ' + response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         // console.log('Data received:', data);
+
+//         const feedContainer = document.getElementById('ig-feed');
+//         data.data.forEach(post => {
+//             const postElement = document.createElement('div');
+//             postElement.className = 'instagram-post';
+//             postElement.innerHTML = `
+//                 <a class="absoluted" href="${post.permalink}" target="_blank">
+//                 <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption} post instagram l'Atelier 8" />
+//                 </a>
+//                 <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
+//             `;
+//             feedContainer.appendChild(postElement);
+//         });
+//     })
+//     .catch(error => console.error('Error fetching Instagram posts:', error));
+
 const apiUrl = '/instagram/posts';
 
 console.log('Fetching data from:', apiUrl);
@@ -25,4 +54,10 @@ fetch(apiUrl)
             feedContainer.appendChild(postElement);
         });
     })
-    .catch(error => console.error('Error fetching Instagram posts:', error));
+    .catch(error => {
+        console.error('Error fetching Instagram posts:', error);
+
+        // ajout d'un message d'erreur dans le feed instagram
+        const errorContainer = document.getElementById('ig-error-container');
+        errorContainer.innerHTML = '<p style="text-align: center;">Une erreur est survenue lors de la récupération des publications Instagram. Veuillez rafraîchir votre page.</p>'
+    });    
