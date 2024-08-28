45237072dca9ee6cc736ac90d150884f7b1d8d36
Ni7173 <nino.bravais20@gmail.com>
2024-08-28 10:50:41 +0200

    insta display on home.html

---
+++ js/en-insta.js
@@ -0,0 +1,32 @@
+const apiUrl = '../instagram/posts';
+
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        // console.log('Data received:', data);
+
+        const feedContainer = document.getElementById('ig-feed');
+        data.data.forEach(post => {
+            const postElement = document.createElement('div');
+            postElement.className = 'instagram-post';
+            postElement.innerHTML = `
+                <a class="absoluted" href="${post.permalink}" target="_blank">
+                <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption} post instagram l'Atelier 8" />
+                </a>
+                <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
+            `;
+            feedContainer.appendChild(postElement);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching Instagram posts:', error);
+
+        // ajout d'un message d'erreur dans le feed instagram
+        const errorContainer = document.getElementById('ig-error-container');
+        errorContainer.innerHTML = '<p style="text-align: center;"><i>Error fetching instagram posts, please refresh your page to see the content.</i></p>'
+    });    
