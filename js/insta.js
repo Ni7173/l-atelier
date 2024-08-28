90cc8d88e0ab1d5a851408148548e7d4bb144183
Ni7173 <nino.bravais20@gmail.com>
2024-08-28 13:53:41 +0200

    test affichage posts à la demande

--- js/insta.js
+++ js/insta.js
@@ -1,3 +1,36 @@
+// const apiUrl = '../instagram/posts';
+
+// fetch(apiUrl)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok ' + response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+
+//         const feedContainer = document.getElementById('ig-feed');
+//         data.data.forEach(post => {
+//             const postElement = document.createElement('div');
+//             postElement.className = 'instagram-post';
+//             postElement.innerHTML = `
+//                 <a class="absoluted" href="${post.permalink}" target="_blank">
+//                 <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption} post instagram l'Atelier 8" />
+//                 </a>
+//                 <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
+//             `;
+//             feedContainer.appendChild(postElement);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error fetching Instagram posts:', error);
+
+//         // ajout d'un message d'erreur dans le feed instagram
+//         const errorContainer = document.getElementById('ig-error-container');
+//         errorContainer.innerHTML = '<p style="text-align: center;"><i>Une erreur est survenue lors de la récupération des publications Instagram. Veuillez rafraîchir votre page.</i></p>'
+//     });
+
+
 const apiUrl = '../instagram/posts';
 
 fetch(apiUrl)
@@ -10,16 +43,38 @@ fetch(apiUrl)
     .then(data => {
 
         const feedContainer = document.getElementById('ig-feed');
-        data.data.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.className = 'instagram-post';
-            postElement.innerHTML = `
-                <a class="absoluted" href="${post.permalink}" target="_blank">
-                <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption} post instagram l'Atelier 8" />
-                </a>
-                <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
-            `;
-            feedContainer.appendChild(postElement);
+
+        let postsDisplayed = 0;
+        const initialPostsToShow = 3;
+        const showMoreButton = document.querySelector('.ig__show__more__button');
+
+        const displayPosts = (postsToShow) => {
+            const posts = data.data.slice(postsDisplayed, postsDisplayed + postsToShow);
+
+            posts.forEach(post => {
+                const postElement = document.createElement('div');
+                postElement.className = 'instagram-post';
+                postElement.innerHTML = `
+                    <a class="absoluted" href="${post.permalink}" target="_blank">
+                    <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption} post instagram l'Atelier 8" />
+                    </a>
+                    <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
+                `;
+                feedContainer.appendChild(postElement);
+            });
+
+            postsDisplayed += postsToShow;
+
+            // Si tous les posts sont affichés, on cache le bouton
+            // if (postsDisplayed >= data.data.length) {
+            //     showMoreButton.innerText = "Afficher moins";
+            // }
+        };
+
+        displayPosts(initialPostsToShow);
+
+        showMoreButton.addEventListener('click', () => {
+            displayPosts(1);
         });
     })
     .catch(error => {
@@ -28,4 +83,11 @@ fetch(apiUrl)
         // ajout d'un message d'erreur dans le feed instagram
         const errorContainer = document.getElementById('ig-error-container');
         errorContainer.innerHTML = '<p style="text-align: center;"><i>Une erreur est survenue lors de la récupération des publications Instagram. Veuillez rafraîchir votre page.</i></p>'
+
+        // retrait du bouton show more
+        const showMoreButton = document.querySelector('.ig__show__more__button');
+        console.log(showMoreButton)
+        if (showMoreButton) {
+            showMoreButton.classList.add('hidden');
+        }
     });    
