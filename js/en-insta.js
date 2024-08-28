22262d32449e8b789293dc56d99bc5a7757737fb
Ni7173 <nino.bravais20@gmail.com>
2024-08-28 14:14:30 +0200

    insta clickable en

--- js/en-insta.js
+++ js/en-insta.js
@@ -8,19 +8,72 @@ fetch(apiUrl)
         return response.json();
     })
     .then(data => {
-        // console.log('Data received:', data);
-
         const feedContainer = document.getElementById('ig-feed');
-        data.data.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.className = 'instagram-post';
-            postElement.innerHTML = `
-                <a class="absoluted" href="${post.permalink}" target="_blank">
-                <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption} post instagram l'Atelier 8" />
-                </a>
-                <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
-            `;
-            feedContainer.appendChild(postElement);
+
+        let postsDisplayed = 0;
+
+        let initialPostsToShow = 4;
+        let postsToAdd = 2;
+
+        if (window.innerWidth > 599) {
+            initialPostsToShow = 6;
+            postsToAdd = 3;
+        }
+
+        if (window.innerWidth > 1399) {
+            initialPostsToShow = 10;
+            postsToAdd = 5;
+        }
+
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth < 599) {
+                initialPostsToShow = 4;
+                postsToAdd = 2;
+            }
+
+            if (window.innerWidth > 599) {
+                initialPostsToShow = 6;
+                postsToAdd = 3;
+            }
+
+            if (window.innerWidth > 1399) {
+                initialPostsToShow = 10;
+                postsToAdd = 5;
+            }
+        })
+
+
+
+        const showMoreButton = document.querySelector('.ig__show__more__button');
+
+        const displayPosts = (postsToShow) => {
+
+            const posts = data.data.slice(postsDisplayed, postsDisplayed + postsToShow);
+
+            posts.forEach(post => {
+                const postElement = document.createElement('div');
+                postElement.className = 'instagram-post';
+                postElement.innerHTML = `
+                    <a class="absoluted" href="${post.permalink}" target="_blank">
+                    <img class="object__fit-cover" src="${post.media_url}" alt="${post.caption} post instagram l'Atelier 8" />
+                    </a>
+                    <img class="ig__post__icon" src="../img/INSTA.svg" alt="instagram icon"/>
+                `;
+                feedContainer.appendChild(postElement);
+            });
+
+            postsDisplayed += postsToShow;
+
+            // Si tous les posts sont affichés, on cache le bouton
+            if (postsDisplayed >= data.data.length) {
+                showMoreButton.classList.add('hidden');
+            }
+        };
+        displayPosts(initialPostsToShow);
+
+        showMoreButton.addEventListener('click', () => {
+            displayPosts(postsToAdd);
         });
     })
     .catch(error => {
