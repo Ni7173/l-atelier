b9e6483c1944ade82f2c3166c338c6d42518a3f3
Ni7173 <nino.bravais20@gmail.com>
2024-09-12 18:14:25 +0200

    insta title padding

---
+++ js/about.js
@@ -0,0 +1,66 @@
+const reviewSlider = () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const slides = document.querySelector('.reviews__list');
+        const dots = document.querySelectorAll('.dot');
+        const reviewsContainer = document.querySelector('.reviews__container')
+        const switchBtns = document.querySelectorAll('[data-slider-button]');
+        let newIndex = 0;
+
+        const calculatePosition = (newIndex) => {
+            return `${315 - 90 * newIndex}%`;
+        };
+
+        const setReview = (position) => {
+            dots.forEach(dot => dot.classList.remove('active'));
+            dots[newIndex].classList.add('active');
+            slides.style.transform = `translateX(${position})`;
+        };
+
+        setReview(calculatePosition(newIndex));
+
+        switchBtns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                newIndex = (newIndex + (btn.dataset.sliderButton === "prev" ? -1 : 1) + dots.length) % dots.length;
+                setReview(calculatePosition(newIndex));
+            });
+        });
+
+        dots.forEach(dot => {
+            dot.addEventListener('click', () => {
+                newIndex = parseInt(dot.dataset.dotNumber, 10);
+                setReview(calculatePosition(newIndex));
+            });
+        });
+
+        const mobileSwipe = () => {
+            let startX = 0;
+            let endX = 0;
+
+
+            reviewsContainer.addEventListener('touchstart', (event) => {
+                startX = event.touches[0].clientX;
+            });
+
+            reviewsContainer.addEventListener('touchmove', (event) => {
+                endX = event.touches[0].clientX;
+            });
+
+            reviewsContainer.addEventListener('touchend', () => {
+                if (startX > endX + 50) {
+                    newIndex++;
+                    if (newIndex >= dots.length) newIndex = 0;
+                    if (newIndex < 0) newIndex = dots.length - 1;
+                    setReview(calculatePosition(newIndex));
+                } else if (startX < endX - 50) {
+                    newIndex = newIndex - 1;
+                    if (newIndex >= dots.length) newIndex = 0;
+                    if (newIndex < 0) newIndex = dots.length - 1;
+                    setReview(calculatePosition(newIndex));
+                }
+            });
+        };
+        mobileSwipe();
+    });
+};
+
+reviewSlider();
